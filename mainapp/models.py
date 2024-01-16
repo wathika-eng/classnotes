275bcbddbcbb725940dd9e1c349cbd834372816d
wathika-eng <wathika02@gmail.com>
2024-01-16 19:40:51 +0300

    cloudinary rm

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,44 +1,42 @@
 import os
-from cloudinary_storage.storage import RawMediaCloudinaryStorage
+
+# from cloudinary_storage.storage import RawMediaCloudinaryStorage
 from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 
 DEPARTMENT_CHOICES = [
-    ('A', 'Department A'),
-    ('B', 'Department B'),
+    ("A", "Department A"),
+    ("B", "Department B"),
     # Add more departments as needed
 ]
 
 YEAR_CHOICES = [
-    ("1", 'First Year'),
-    ("2", 'Second Year'),
-    ("3", 'Third Year'),
-    ("4", 'Fourth Year'),
+    ("1", "First Year"),
+    ("2", "Second Year"),
+    ("3", "Third Year"),
+    ("4", "Fourth Year"),
     # Add more years as needed
 ]
 
-SEM_CHOICES = (
-    ("1", "First Semester"),
-    ("2", "Second Semester")
-)
+SEM_CHOICES = (("1", "First Semester"), ("2", "Second Semester"))
 
 # Choices for Courses
 COURSE_CHOICES = [
-    ('A1', 'Course A1'),
-    ('A2', 'Course A2'),
-    ('B1', 'Course B1'),
-    ('B2', 'Course B2'),
+    ("A1", "Course A1"),
+    ("A2", "Course A2"),
+    ("B1", "Course B1"),
+    ("B2", "Course B2"),
     # Add more courses as needed
 ]
 
 
 def note_file_path(instance, filename):
     # Generate the file path based on the title and original file extension
-    ext = filename.split('.')[-1]
+    ext = filename.split(".")[-1]
     filename = f"{instance.title}.{ext}"
-    return os.path.join('media/', filename)
+    return os.path.join("media/", filename)
 
 
 class College(models.Model):
@@ -51,7 +49,9 @@ class College(models.Model):
 # Model for School
 class School(models.Model):
     name = models.CharField(max_length=50)
-    college = models.ForeignKey(College, on_delete=models.CASCADE, related_name='schools')
+    college = models.ForeignKey(
+        College, on_delete=models.CASCADE, related_name="schools"
+    )
 
     def __str__(self):
         return self.name
@@ -60,7 +60,9 @@ class School(models.Model):
 # Model for Department
 class Department(models.Model):
     name = models.CharField(max_length=50)
-    school = models.ForeignKey(School, on_delete=models.CASCADE, related_name='departments')
+    school = models.ForeignKey(
+        School, on_delete=models.CASCADE, related_name="departments"
+    )
 
     def __str__(self):
         return self.name
@@ -69,7 +71,9 @@ class Department(models.Model):
 # Model for Course
 class Course(models.Model):
     name = models.CharField(max_length=50)
-    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='courses')
+    department = models.ForeignKey(
+        Department, on_delete=models.CASCADE, related_name="courses"
+    )
 
     def get_display_name(self):
         """
@@ -81,34 +85,41 @@ class Course(models.Model):
         return self.get_display_name()
 
     class Meta:
-        unique_together = ['name', 'department']
+        unique_together = ["name", "department"]
 
 
 # Model for Units
 class Unit(models.Model):
     name = models.CharField(max_length=50)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='units')
-    year_of_study = models.CharField(max_length=20, null=True, choices=YEAR_CHOICES)  # Added year of study
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="units")
+    year_of_study = models.CharField(
+        max_length=20, null=True, choices=YEAR_CHOICES
+    )  # Added year of study
     sem = models.CharField(max_length=20, null=True, choices=SEM_CHOICES)
-    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='units')
+    uploaded_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="units"
+    )
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def get_display_name(self):
-        return f"{self.course.name.upper()}: {self.name} {self.year_of_study}.{self.sem}"
+        return (
+            f"{self.course.name.upper()}: {self.name} {self.year_of_study}.{self.sem}"
+        )
 
     def __str__(self):
         return self.get_display_name()
 
     class Meta:
-        unique_together = ['name', 'course', 'year_of_study']
+        unique_together = ["name", "course", "year_of_study"]
 
 
 class UnitTopic(models.Model):
     """
     Add a Unit Topic name eg: Linear Regression
     """
+
     name = models.CharField(max_length=50)
-    unit = models.ForeignKey(Unit, on_delete=models.CASCADE, related_name='unit_topics')
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE, related_name="unit_topics")
 
     def get_display_name(self):
         return f"{self.unit.name.upper()}:{self.name}"
@@ -124,13 +135,16 @@ class Note(models.Model):
     """
     Takes in a Note file for a particular unit's topic
     """
+
     title = models.CharField(max_length=50, null=True, blank=True)
-    unit = models.ForeignKey(Unit, on_delete=models.CASCADE, null=True, blank=False, related_name='notes')
-    file = models.FileField(upload_to='media/')
+    unit = models.ForeignKey(
+        Unit, on_delete=models.CASCADE, null=True, blank=False, related_name="notes"
+    )
+    file = models.FileField(upload_to="media/")
     uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
     uploaded_at = models.DateTimeField(auto_now_add=True)
     status = models.BooleanField(default=False)
-    
+
     #     def get_display_name(self):
     #     file_name = os.path.splitext(os.path.basename(self.file.name))[0]
     #     return file_name if self.title is None else self.title
@@ -148,10 +162,10 @@ class Note(models.Model):
 
     class Meta:
         unique_together = (
-            ('title', 'unit'),
-            ('title', 'file'),
-            ('unit', 'file'),
-            ('title', 'unit', 'file'),
+            ("title", "unit"),
+            ("title", "file"),
+            ("unit", "file"),
+            ("title", "unit", "file"),
         )
 
     def save(self, *args, **kwargs):
@@ -159,7 +173,7 @@ class Note(models.Model):
             # Extract filename without extension and set it as title
             self.title = os.path.splitext(os.path.basename(self.file.name))[0]
         # Update the file name before saving
-        if self.file.name != '':
+        if self.file.name != "":
             self.file.name = note_file_path(self, os.path.basename(self.file.name))
         super().save(*args, **kwargs)
 
--- mainproject/settings.py
+++ mainproject/settings.py
@@ -1,4 +1,5 @@
 import os
+
 # from dotenv import load_dotenv
 import environ
 from pathlib import Path
@@ -25,35 +26,35 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '****************hidden*************************************'
+SECRET_KEY = "****************hidden*************************************"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True # os.environ.get('DEBUG', False) == 'True'
+DEBUG = True  # os.environ.get('DEBUG', False) == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ["127.0.0.1", "*"]
 
 # CSRF_TRUSTED_ORIGINS = [
-   
+
 # ]
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'mainapp.apps.MainappConfig',
-    'cloudinary_storage',  #Refer to https://pypi.org/project/django-cloudinary-storage/ documentation
-    'cloudinary',
+    "jazzmin",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "mainapp.apps.MainappConfig",
+    # "cloudinary_storage",  # Refer to https://pypi.org/project/django-cloudinary-storage/ documentation
+    # "cloudinary",
     "crispy_forms",
     "crispy_bootstrap5",
-    'django_daraja',
+    "django_daraja",
 ]
 
 # CLOUDINARY_STORAGE = {
@@ -67,46 +68,45 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+    "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.memcached.PyMemcacheCache",
-        "LOCATION": '',
+        "LOCATION": "",
     }
 }
 
-ROOT_URLCONF = 'mainproject.urls'
-LOGIN_URL = 'login'
+ROOT_URLCONF = "mainproject.urls"
+LOGIN_URL = "login"
 LOGIN_REDIRECT = "login"
-LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_REDIRECT_URL = "dashboard"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'mainapp/templates']
-        ,
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "mainapp/templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'mainproject.wsgi.application'
+WSGI_APPLICATION = "mainproject.wsgi.application"
 
 # The email backend to use. For possible shortcuts see django.core.mail.
 # The default is to use the SMTP backend.
@@ -127,14 +127,14 @@ EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_SSL_CERTFILE = None
 EMAIL_TIMEOUT = None
-DEFAULT_FROM_EMAIL = 'Uninotes <noreply@projectsdjango.com>'
+DEFAULT_FROM_EMAIL = "Uninotes <noreply@projectsdjango.com>"
 
-#https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 # DATABASES = {
@@ -159,26 +159,26 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'Africa/Nairobi'
+TIME_ZONE = "Africa/Nairobi"
 
 USE_I18N = True
 
@@ -187,60 +187,58 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
-MEDIA_URL = 'media/'
+MEDIA_URL = "media/"
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.RawMediaCloudinaryStorage'
+# DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.RawMediaCloudinaryStorage"
 
 # STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 if DEBUG:
-
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'mainapp/static')]
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "mainapp/static")]
 
 else:
+    STATIC_ROOT = os.path.join(BASE_DIR, "mainapp/static")
 
-    STATIC_ROOT = os.path.join(BASE_DIR, 'mainapp/static')
-    
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-MPESA_ENVIRONMENT = ''
+MPESA_ENVIRONMENT = ""
 
 # Credentials for the daraja app
 
-MPESA_CONSUMER_KEY = ''
-MPESA_CONSUMER_SECRET = ''
+MPESA_CONSUMER_KEY = ""
+MPESA_CONSUMER_SECRET = ""
 
 # Shortcode to use for transactions. For sandbox  use the Shortcode 1 provided on test credentials page
 
-MPESA_SHORTCODE = ''
+MPESA_SHORTCODE = ""
 
 # Shortcode to use for Lipa na MPESA Online (MPESA Express) transactions
 # This is only used on sandbox, do not set this variable in production
 # For sandbox use the Lipa na MPESA Online Shorcode provided on test credentials page
 
-MPESA_EXPRESS_SHORTCODE = ''
+MPESA_EXPRESS_SHORTCODE = ""
 
 # Type of shortcode
 # Possible values:
 # - paybill (For Paybill)
 # - till_number (For Buy Goods Till Number)
 
-MPESA_SHORTCODE_TYPE = ''
+MPESA_SHORTCODE_TYPE = ""
 
 # Lipa na MPESA Online passkey
 # Sandbox passkey is available on test credentials page
 # Production passkey is sent via email once you go live
 
-MPESA_PASSKEY = ''
+MPESA_PASSKEY = ""
 
 # Username for initiator (to be used in B2C, B2B, AccountBalance and TransactionStatusQuery Transactions)
 
-MPESA_INITIATOR_USERNAME = 'testapi'
+MPESA_INITIATOR_USERNAME = "testapi"
 
 # Plaintext password for initiator (to be used in B2C, B2B, AccountBalance and TransactionStatusQuery Transactions)
 
-MPESA_INITIATOR_SECURITY_CREDENTIAL = ''
+MPESA_INITIATOR_SECURITY_CREDENTIAL = ""
