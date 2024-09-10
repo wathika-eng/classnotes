5219f390bf42420fc8676693604da12d8309ab3f
wathika-eng <wathika02@gmail.com>
2024-09-10 15:40:29 +0300

    restructure_frontend

---
+++ client/app/(default)/page.tsx
@@ -0,0 +1,22 @@
+export const metadata = {
+  title: 'Kenya Uninotes',
+  description: 'Open a new Page',
+}
+
+import Hero from '@/components/hero'
+import Features from '@/components/features'
+import Newsletter from '@/components/newsletter'
+import Zigzag from '@/components/zigzag'
+import Testimonials from '@/components/testimonials'
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Features />
+      {/* <Zigzag />
+      <Testimonials />
+      <Newsletter /> */}
+    </>
+  )
+}
